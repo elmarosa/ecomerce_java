d67916a68750320ba2e5db18fe8e39bd664e3acc
elmarosa <elmayuliarosa@gmail.com>
2023-05-30 10:20:26 +0800

    main

--- Main.java
+++ Main.java
@@ -1,5 +1,3 @@
-package org.example;
-
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
@@ -13,7 +11,7 @@ import java.util.concurrent.Executors;
 public class Main {
         public static void main(String[] args) throws IOException
     {
-        HttpServer httpServer = HttpServer.create(new InetSocketAddress("localhost", 135),0);
+        HttpServer httpServer = HttpServer.create(new InetSocketAddress("localhost", 8135),0);
         httpServer.createContext( "/ecommerce", new ecommerceHandler());
         httpServer.setExecutor(Executors.newSingleThreadExecutor());
         httpServer.start();
